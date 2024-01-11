eeb3288bb2f7debd4d266721c116bacb3354051f
forestheims <forestheims@gmail.com>
2024-01-12 00:49:53 +1100

    add user route base files. needs work. not yet functional.

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// 
+const app = require('./lib/app');
+const pool = require('./lib/utils/pool');
+
+const API_URL = process.env.API_URL || 'http://localhost';
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  console.log(`🦊  Server started on ${API_URL}:${PORT}`);
+});
+
+process.on('exit', () => {
+  console.log('🦊  Goodbye!');
+  pool.end();
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,1 +1,16 @@
--- 
+-- so many errors. need to look into changes and updates of PostgreSQL for cascade, identity, time zone, ......
+
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    google_id VARCHAR(255) UNIQUE,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    profile_picture_url VARCHAR(255),
+    password_hash_value VARCHAR(255) NOT NULL,
+    -- Include other fields as needed based on the OpenID scopes
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
